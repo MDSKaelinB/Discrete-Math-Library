5b6899e04b3fa5c0d6a2071213e2b35df7cc0196
Kaelin Burt <Kaelin.Burt@mds.ac.nz>
2026-06-15 17:05:40 +1200

    First Update

--- DiscreteMathLibrary/discretemaths.cpp
+++ DiscreteMathLibrary/discretemaths.cpp
@@ -1,1 +1,10 @@
 #include "discretemaths.h"
+float Magnitude(const float _kfX)
+{
+	if (_kfX < 0.0f) //if negative
+	{
+		return _kfX * -1.0f; //make it positive and return it
+	}
+	//else already positive
+	return _kfX;
+}
