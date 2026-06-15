3b62ffdb6efddd073b049b1c34b0655272fe5b90
Kaelin Burt <Kaelin.Burt@mds.ac.nz>
2026-06-15 16:30:02 +1200

    Skeleton FIles

---
+++ DiscreteMathLibrary/main.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************
+Bachelor of Software Engineering
+Media Design School,
+Auckland, New Zealand
+(c) 2026 Media Design School
+Author      :   Kaelin Burt
+Mail        :   Kaelin.Burt@mds.ac.nz
+File Name   :	main.cpp
+Description :	Main file for Discrete Function Library
+**************************************************************************/
+int main()
+{
+    // We will leave this empty for now until we are ready to test.
+    return 0;
+}
