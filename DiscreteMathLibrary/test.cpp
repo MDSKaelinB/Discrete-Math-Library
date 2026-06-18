dae1f8e8981503f8ee83a6b5cba0873f294512e1
Kaelin Burt <Kaelin.Burt@mds.ac.nz>
2026-06-18 17:35:07 +1200

    Small Update

--- DiscreteMathLibrary/test.cpp
+++ DiscreteMathLibrary/test.cpp
@@ -1,15 +1,27 @@
 #include "test.h"
+//constructor
+test::test()
+{
+	m_uiTestsRun = 0;
+	m_uiTestsPassed = 0;
+}
+//destructor
+test::~test()
+{
+
+}
 void test::RunAllTests()
 {
-	TestBasicNumberMath();
+	TestBasicMath();
 	TestCountingAndProbability();
 	TestSetRelationships();
 	TestSetOperations();
 }
-
-void test::TestBasicNumberMath()
+//specific tests
+void test::TestBasicMath()
 {
-
+	AssertTest("Magnitude negative input", Magnitude(-5.0f) == 5.0f);
+	AssertTest("Magnitude positive input", Magnitude(10.0f) == 10.0f);
 }
 void test::TestCountingAndProbability()
 {
--- DiscreteMathLibrary/test.h
+++ DiscreteMathLibrary/test.h
@@ -8,18 +8,15 @@ public:
 
 	//calls all different tests one at a time
 	void RunAllTests();
-
 private:
 	//tracks total number of tests cases
 	unsigned int m_uiTestsRun;
 	//tracks total number of cases successfully passed
 	unsigned int m_uiTestsPassed;
-
 	//prints the test name checks if condition is true and automatically updates the tracking countersa
 	void AssertTest(const std::string& _kstrTestName, bool _bCondition);
-
 	//tests
-	void TestBasicNumberMath(); //absolute values signing floors and ceiling
+	void TestBasicMath(); //absolute values signing floors and ceiling
 	void TestCountingAndProbability(); //factorials permutations and combinations
 	void TestSetRelationships(); //cardinalit subsets supersets and equality
 	void TestSetOperations(); //union intesection difference compliment
