056adacbb8499847310337773b95076c248f934c
Kaelin Burt <Kaelin.Burt@mds.ac.nz>
2026-06-18 20:02:20 +1200

    Basic Math tests

---
+++ DiscreteMathLibrary/main.cpp
@@ -0,0 +1,17 @@
+/***********************************************************************
+Bachelor of Software Engineering - Game Programming
+Media Design School, Auckland, New Zealand
+(c) 2026 Media Design School
+Author      :   Kaelin Burt
+Mail        :   Kaelin.Burt@mds.ac.nz
+File Name   :	main.cpp
+Description :	The main file for the project
+**************************************************************************/
+
+#include <iostream>
+#include "test.h"
+
+int main()
+{
+	return 0;
+}
--- DiscreteMathLibrary/test.cpp
+++ DiscreteMathLibrary/test.cpp
@@ -20,8 +20,19 @@ void test::RunAllTests()
 //specific tests
 void test::TestBasicMath()
 {
+	//magnitude
 	AssertTest("Magnitude negative input", Magnitude(-5.0f) == 5.0f);
 	AssertTest("Magnitude positive input", Magnitude(10.0f) == 10.0f);
+	//signum
+	AssertTest("Signum negative input", Signum(-10.0f) == -1.0f);
+	AssertTest("Signum positive input", Signum(10.0f) == 1.0f);
+	AssertTest("Signum zero input", Signum(0.0f) == 0.0f);
+	//floor
+	AssertTest("Floor positive float", Floor(5.9f) == 5.0f);
+	AssertTest("Floor negative float", Floor(-3.2f) == -4.0f);
+	//ceiling
+	AssertTest("Ceiling positive float", Ceiling(1.1f) == 2.0f);
+	AssertTest("Ceiling negative float", Ceiling(-5.5f) == -4.0f);
 }
 void test::TestCountingAndProbability()
 {
