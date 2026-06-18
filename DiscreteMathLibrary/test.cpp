a979fff393727493ffbd43ebbe5821aa0af29e4a
Kaelin Burt <Kaelin.Burt@mds.ac.nz>
2026-06-18 21:26:37 +1200

    Better Console Printing

--- DiscreteMathLibrary/test.cpp
+++ DiscreteMathLibrary/test.cpp
@@ -21,32 +21,30 @@ void test::RunAllTests()
 void test::TestBasicMath()
 {
 	//magnitude
-	AssertTest("Magnitude negative input", Magnitude(-5.0f) == 5.0f);
-	AssertTest("Magnitude positive input", Magnitude(10.0f) == 10.0f);
+	AssertTest("|-5.0|", Magnitude(-5.0f) == 5.0f);
+	AssertTest("|5.0|", Magnitude(10.0f) == 10.0f);
 	//signum
-	AssertTest("Signum negative input", Signum(-10.0f) == -1.0f);
-	AssertTest("Signum positive input", Signum(10.0f) == 1.0f);
-	AssertTest("Signum zero input", Signum(0.0f) == 0.0f);
+	AssertTest("sgn(-10.0)", Signum(-10.0f) == -1.0f);
+	AssertTest("sgn(10.0)", Signum(10.0f) == 1.0f);
+	AssertTest("sgn(0.0)", Signum(0.0f) == 0.0f);
 	//floor
-	AssertTest("Floor positive float", Floor(5.9f) == 5.0f);
-	AssertTest("Floor negative float", Floor(-3.2f) == -4.0f);
+	AssertTest("Floor (positive float)", Floor(5.9f) == 5.0f);
+	AssertTest("Floor (negative float)", Floor(-3.2f) == -4.0f);
 	//ceiling
-	AssertTest("Ceiling positive float", Ceiling(1.1f) == 2.0f);
-	AssertTest("Ceiling negative float", Ceiling(-5.5f) == -4.0f);
+	AssertTest("Ceiling (positive float)", Ceiling(1.1f) == 2.0f);
+	AssertTest("Ceiling (negative float)", Ceiling(-5.5f) == -5.0f);
 }
 void test::TestCountingAndProbability()
 {
 	//factorial
-	AssertTest("Factorial base case", Factorial(4) == 24ULL);
-	AssertTest("Factorial zero edge case", Factorial(0) == 1ULL);
-
+	AssertTest("4!", Factorial(4) == 24ULL);
+	AssertTest("0!", Factorial(0) == 1ULL);
 	//permutation
-	AssertTest("Permutation valid calculation", Permutation(5, 2) == 20ULL);
-	AssertTest("Permutation out of bounds error", Permutation(2, 5) == 0ULL);
-
+	AssertTest("P(5, 2)", Permutation(5, 2) == 20ULL);
+	AssertTest("P(2, 5) Error Case", Permutation(2, 5) == 0ULL);
 	//combination
-	AssertTest("Combination valid calculation", Combination(5, 2) == 10ULL);
-	AssertTest("Combination out of bounds error", Combination(2, 5) == 0ULL);
+	AssertTest("C(5, 2)", Combination(5, 2) == 10ULL);
+	AssertTest("C(2, 5) Error Case", Combination(2, 5) == 0ULL);
 }
 void test::TestSetRelationships()
 {
