dbb917a85fe582790af73b80d09288704841ef03
Kaelin Burt <Kaelin.Burt@mds.ac.nz>
2026-06-18 14:13:53 +1200

    test.cpp setup

--- DiscreteMathLibrary/test.cpp
+++ DiscreteMathLibrary/test.cpp
@@ -1,1 +1,25 @@
 #include "test.h"
+void test::RunAllTests()
+{
+	TestBasicNumberMath();
+	TestCountingAndProbability();
+	TestSetRelationships();
+	TestSetOperations();
+}
+
+void test::TestBasicNumberMath()
+{
+
+}
+void test::TestCountingAndProbability()
+{
+
+}
+void test::TestSetRelationships()
+{
+
+}
+void test::TestSetOperations()
+{
+
+}
